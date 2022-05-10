06f1459a79e988b79b3f3f2a4aebb0fb739f42ae
sungwon kim <44858739+kimsungwon1@users.noreply.github.com>
2022-05-10 17:57:24 +0900

    git commit

---
+++ src/main/java/com/kimm/corder/RegisterRequest.java
@@ -0,0 +1,26 @@
+package com.kimm.corder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+
+
+public class RegisterRequest extends StringRequest {
+    final static private String URL = "http://10.0.2.2/UserInfo.php";
+    private Map<String, String> parameters;
+
+    public RegisterRequest(String userName, String userID, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userName", userName);
+        parameters.put("userID", userID);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
