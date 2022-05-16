605bbddcbd16cbc7397a0e0ea4fbba363ac12c22
sungwon kim <44858739+kimsungwon1@users.noreply.github.com>
2022-05-16 20:41:10 +0900

    kims

--- app/src/main/java/com/example/corder/MainActivity.kt
+++ app/src/main/java/com/example/corder/MainActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.content.ContextCompat
+import com.example.corder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val goLoginbtn = findViewById<Button>(R.id.goLogin)
 
@@ -17,11 +23,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
         }
 
-//        var list = ArrayList<ListData>()
-//        list.add(ListData(ContextCompat.getDrawable(this, R.drawable.coffee)!!, 1, "coffee"))
-//
-//        val adapter = RecyclerItemAdapter(list)
-//        listCafe.adapter = adapter
+        var list = ArrayList<ListData>()
+        list.add(ListData(ContextCompat.getDrawable(this, R.drawable.coffee)!!, 1, "coffee"))
+
+        val adapter = RecyclerItemAdapter(list)
+        binding.listCafe.adapter = adapter
 
     }
 }
